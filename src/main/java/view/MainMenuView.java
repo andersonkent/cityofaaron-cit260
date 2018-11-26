2b9d321ca0dc00ac7ad951b02c62e1af16fe287d
Kent Anderson <kent@Unknown.domain>
2018-11-26 06:14:06 -0700

    Created MenuView

--- src/main/java/view/MainMenuView.java
+++ src/main/java/view/MainMenuView.java
@@ -5,96 +5,42 @@ package view;
  *
  * @author kanderson
  */
-public class MainMenuView extends ViewBase {
-    
-    
+public class MainMenuView extends MenuView {
+
     /**
      * Constructor
      */
-    public MainMenuView(){
+    public MainMenuView() {
         super();
     }
 
-    public String getMessage(){
-        
-        return
-            "MAIN MENU\n"
-            + "N - Start a New Game\n"
-            + "L - Load a Saved Game\n"
-            + "H - Help Menu\n"
-            + "Q - Quit the Program\n";
-    }
-    
-    
-    /**
-     * Get the set of inputs from the user.
-     * @return 
-     */
-    public String[] getInputs() {
-        
-        // Declare the array to have the number of elements you intend to get 
-        // from the user.
-        String[] inputs = new String[1];
-        
-        inputs[0] = getUserInput(
-            "Please make your selection:", 
-            "Please select a valid menu option\n",
-            ViewBase::notNullOrWhitespace,
-            s -> ViewBase.in(s.toUpperCase(), "N", "L", "H", "Q"));
-        
-        return inputs;
-    }
-    
-    
-    /**
-     * Perform the action indicated by the user's input.
-     * @param inputs
-     * @return true if the view should repeat itself, and false if the view
-     * should exit and return to the previous view.
-     */
-    public boolean doAction(String[] inputs){
-
-        // keep going unless some menu option below changes it.
-        boolean keepGoing = true;
-
-        if (inputs == null || inputs[0] == null){
-            // no input = keep going until they provide input
-            return keepGoing;
-        }
-
-        switch(inputs[0].toUpperCase()) {
-            case "N":
-                newGame();
-                break;
-            case "L":
-                loadGame();
-                break;
-            case "H":
-                helpMenu();
-                break;
-            case "Q":
-                keepGoing = false;
-                break;
-        }
-        
-        return keepGoing;
-    }
-    
-    
-    // Define your action handlers here. These are the methods that your doAction()
-    // method will call based on the user's input. We don't want to do a lot of 
-    // complex game stuff in our doAction() method. It will get messy very quickly.
-    
-    
-    private void newGame(){
-        new NewGameView().displayView();
+    @Override
+    public String getMenuIntroduction() {
+        return "MAIN MENU";
     }
 
-    private void loadGame(){
-        System.out.println("loadGame called.");
+    @Override
+    public MenuItem[] getMenuItems() {
+        return new MenuItem[] {
+            new MenuItem("N", "Start a new game", () -> {
+                new NewGameView().displayView();
+                return true;
+            }),
+
+            new MenuItem("L", "Load a saved game", () -> {
+                System.out.println("Load game called.");
+                return true;
+            }),
+
+            new MenuItem("H", "Help menu", () -> {
+                System.out.println("Help menu called.");
+                return true;
+            }),
+
+            new MenuItem("Q", "Quit the program", () -> {
+                // keep going = false
+                return false;
+            }) };
     }
 
-    private void helpMenu(){
-        System.out.println("helpMenu called.");
-    }
 }
---
+++ src/main/java/view/MenuView.java
@@ -0,0 +1,85 @@
+package view;
+
+import java.util.function.BooleanSupplier;
+
+public abstract class MenuView extends ViewBase {
+
+    protected class MenuItem {
+        public String menuId;
+        public String description;
+        public BooleanSupplier menuHandler;
+
+        public MenuItem(String menuId, String description, BooleanSupplier handler){
+            this.menuId = menuId;
+            this.description = description;
+            this.menuHandler = handler;
+        }
+    }
+
+    private MenuItem[] menuItems = null;
+
+
+    public abstract String getMenuIntroduction();
+    public abstract MenuItem[] getMenuItems();
+
+    public MenuView() {
+        super();
+        menuItems = getMenuItems();
+    }
+
+    @Override
+    public String getMessage() {
+        StringBuilder menu = new StringBuilder();
+        
+        String intro = getMenuIntroduction();
+        if (intro != null){
+            menu.append(intro);
+            menu.append("\n\n");
+        }
+
+        for (MenuItem menuItem : menuItems) {
+            menu.append(menuItem.menuId).append(" - ").append(menuItem.description).append("\n");
+        }
+
+        menu.append("\n");
+        return menu.toString();
+    }
+
+    @Override
+    public String[] getInputs() {
+        String[] inputs = new String[1];
+        inputs[0] = getUserInput("Please enter your selection:",
+            input -> {
+                String inputTrimmed = input.trim().toUpperCase();
+                boolean found = false;
+                for (MenuItem item : menuItems){
+                    if (item.menuId.toUpperCase().equals(inputTrimmed)){
+                        found = true;
+                        break;
+                    }
+                }
+                if (found == false){
+                    return false;
+                }
+                return true;
+            });
+        return inputs;
+    }
+
+    @Override
+    public boolean doAction(String[] inputs) {
+
+        String inputTrimmed = inputs[0].trim().toUpperCase();
+
+        // Find the menu item and call its handler.
+        for(MenuItem menuItem : menuItems){
+            if (menuItem.menuId.toUpperCase().equals(inputTrimmed)){
+                return menuItem.menuHandler.getAsBoolean();
+            }
+        }
+
+        // Nothing handled it, so return "keep going"
+        return true;
+    }
+
+}
